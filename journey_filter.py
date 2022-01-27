e4492d5ae885747a11db62bc56225d30097edceb
Csaba <baicsaba1@gmail.com>
2022-01-27 14:09:15 +0100

    add time check and journey filter

---
+++ journey_filter.py
@@ -0,0 +1,38 @@
+from time_check import check_consecutive_flights
+
+# possible_journeys = \
+#     {0:  [{'origin': 'WIW', 'destination': 'ECV', 'departure': '2021-09-01T07:25:00', 'arrival': '2021-09-01T11:00:00'},
+#           {'origin': 'WIW', 'destination': 'ECV', 'departure': '2021-09-02T07:25:00', 'arrival': '2021-09-02T11:00:00'}],
+#      1: [{'origin': 'ECV', 'destination': 'RFZ', 'departure': '2021-09-01T12:10:00', 'arrival': '2021-09-01T14:40:00'},
+#          {'origin': 'ECV', 'destination': 'RFZ', 'departure': '2021-09-01T12:20:00', 'arrival': '2021-09-01T14:50:00'}],
+#      2: [{'origin': 'RFZ', 'destination': 'BUD', 'departure': '2021-09-01T19:10:00', 'arrival': '2021-09-01T18:40:00'},
+#          {'origin': 'RFZ', 'destination': 'BUD', 'departure': '2021-09-01T20:10:00', 'arrival': '2021-09-01T18:40:00'}]
+#      }
+
+
+def search_for_connective_flights(flights_dict):
+    legs_count = len(flights_dict) - 1
+    present_solution = []
+    solutions = []
+    counter = 0
+
+    def inner_loop(ways):
+        nonlocal counter, present_solution
+        for row in ways:
+            present_solution.append(row)
+            if counter == legs_count and check_consecutive_flights(present_solution):
+                solutions.append(present_solution[:])
+            if counter == legs_count and present_solution:
+                present_solution.pop()
+            if counter < legs_count:
+                counter += 1
+                inner_loop(flights_dict[counter])
+        counter -= 1
+        if present_solution:
+            present_solution.pop()
+
+    inner_loop(flights_dict[counter])
+    return solutions
+
+
+# print(search_for_connective_flights(possible_journeys))
--- time_check.py
+++ time_check.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
 
-time_1 = '2021-09-01T11:00:00'
-time_2 = '2021-09-01T15:00:00'
+# time_1 = '2021-09-01T11:00:00'
+# time_2 = '2021-09-01T15:00:00'
+#
+# my_list = [{'origin': 'WIW', 'destination': 'ECV', 'departure': '2021-09-01T07:25:00', 'arrival': '2021-09-01T11:00:00'},
+#            {'origin': 'ECV', 'destination': 'RFZ', 'departure': '2021-09-01T12:10:00', 'arrival': '2021-09-01T14:40:00'},
+#            {'origin': 'RFZ', 'destination': 'BUD', 'departure': '2021-09-01T16:10:00', 'arrival': '2021-09-01T18:40:00'},
+#            {'origin': 'BUD', 'destination': 'LTN', 'departure': '2021-09-01T20:10:00', 'arrival': '2021-09-01T23:40:00'}]
 
 
 def check_within_timeframe(reference, compare):
@@ -20,3 +25,20 @@ def check_within_timeframe(reference, compare):
 
 
 # print(check_within_timeframe(time_1, time_2))
+
+def check_consecutive_flights(list_to_check):
+    times_list = []
+    for line in list_to_check:
+        time_d, time_a = line['departure'], line['arrival']
+        times_list.extend([time_d, time_a])
+    # print(times_list)
+    for i in range(1, len(times_list) - 1, 2):
+        # print(i, i + 1)
+        if not check_within_timeframe(times_list[i], times_list[i + 1]):
+            break
+    else:
+        return True     # only executed if for loop didn't break
+    return False    # only executed if for loop broke
+
+
+# print(check_consecutive_flights(my_list))
