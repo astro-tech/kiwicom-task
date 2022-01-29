88ad5c5e59572ed7a878baa6d252a08ed2b94268
Csaba <baicsaba1@gmail.com>
2022-01-29 14:52:50 +0100

    change layover rule slightly :add 1 hour limit

--- time_check.py
+++ time_check.py
@@ -15,7 +15,7 @@ def check_within_timeframe(reference, compare, layover):
     min_allowed = reference_time + timedelta(hours=1)
     max_allowed = reference_time + timedelta(hours=6)
     if layover:
-        if reference_time < compare_time:       # in case of layover the only limit is flight2 is after flight1
+        if min_allowed < compare_time:       # in case of layover the only limit is flight2 is after flight1 + 1hour
             return True
     else:
         if min_allowed <= compare_time <= max_allowed:
